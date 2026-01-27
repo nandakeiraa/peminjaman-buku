b9cbf1d6b3ae98ac95fafea8628890bbc8843465
Nanda Keira <skm1223455@gmail.com>
2026-01-26 17:39:23 -0800

    membuat authController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json($user);
+    }
+
+    public function login(Request $request) {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Login gagal'], 401);
+        }
+
+        $token = $request->user()->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $request->user()
+        ]);
+    }
+}
