7db155f898178fd9ce4bccb13bd3fd84a2c7b010
Nanda Keira <skm1223455@gmail.com>
2026-01-26 17:47:22 -0800

    membuat adminmiddleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->user() || $request->user()->role !== 'admin') {
+            return response()->json([
+                'message' => 'Akses hanya untuk admin'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
