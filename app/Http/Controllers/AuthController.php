2a9e125a8afc2c655beea148a1d00332b419f5ce
Nanda Keira <skm1223455@gmail.com>
2026-01-26 17:40:42 -0800

    membuat BookController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,3 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
 class AuthController extends Controller
 {
     public function register(Request $request) {
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index() {
+        return Book::all();
+    }
+
+    public function store(Request $request) {
+        return Book::create($request->all());
+    }
+}
